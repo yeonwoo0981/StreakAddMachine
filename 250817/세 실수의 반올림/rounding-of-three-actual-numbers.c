c36ee73d31285365529015802445923312aa1b27
ByulDaram <0981syw@naver.com>
2025-08-17 15:02:09 +0900

    Title: 세 실수의 반올림, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250817/세 실수의 반올림/rounding-of-three-actual-numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    double a;
+    double b;
+    double c;
+
+    scanf("%lf", &a);
+    scanf("%lf", &b);
+    scanf("%lf", &c);
+
+    printf("%.3f \n", a);
+    printf("%.3f \n", b);
+    printf("%.3f \n", c);
+}
