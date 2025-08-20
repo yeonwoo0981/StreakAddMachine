f72c298ed324934f40485804226374ef022f07e7
ByulDaram <0981syw@naver.com>
2025-08-20 11:06:12 +0900

    Title: 물의 상태, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250820/물의 상태/state-of-water.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n < 0)
+    printf("ice");
+    else if (n >= 100)
+    printf("vapor");
+    else
+    printf("water");
+
+    return 0;
+}
